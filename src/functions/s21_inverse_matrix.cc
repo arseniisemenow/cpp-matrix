99a59564ab45ecd38d921fd9bbdf014625b94a8d
Evangelina Marj <evangelm@pr-b2.kzn.21-school.ru>
2024-01-29 16:50:52 +0300

    develop. Refactored s21_inverse_matrix.cc

--- src/functions/s21_inverse_matrix.cc
+++ src/functions/s21_inverse_matrix.cc
@@ -8,8 +8,9 @@ S21Matrix S21Matrix::CalculateInverseMatrix(const S21Matrix &matrix,
 
   for (int row_index = 0; row_index < rows_; ++row_index) {
     for (int col_index = 0; col_index < cols_; ++col_index) {
-      result.matrix_[row_index][col_index] =
-          (1.0 / det) * matrix.matrix_[row_index][col_index];
+        double inverse_term = 1.0 / det;
+        double number = matrix.matrix_[row_index][col_index];
+        result.matrix_[row_index][col_index] = inverse_term * number;
     }
   }
 
@@ -22,17 +23,14 @@ S21Matrix S21Matrix::InverseMatrix() {
   if (IsFirstOrderMatrix()) {
     return GetInverseOfFirstOrderMatrix();
   }
-  //        if (rows_ == cols_ == 1) { //TODO handle this
-  //            return GetInverseOfFirstOrderMatrix();
-  //        }
 
   double det = Determinant();
   CheckSingularMatrix(det);
 
-  S21Matrix complementMatrix = CalcComplements();
-  S21Matrix transposedComplementMatrix = complementMatrix.Transpose();
+  S21Matrix complement_matrix = CalcComplements();
+  S21Matrix transposed_complement_matrix = complement_matrix.Transpose();
 
-  S21Matrix result = CalculateInverseMatrix(transposedComplementMatrix, det);
+  S21Matrix result = CalculateInverseMatrix(transposed_complement_matrix, det);
 
   return result;
 }
