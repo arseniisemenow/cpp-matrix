e2d4192cf08f8d964b21870d7fcd47a829aa6839
Evangelina Marj <evangelm@pr-b2.kzn.21-school.ru>
2024-01-29 16:48:47 +0300

    develop. Refactored s21_determinant.cc

--- src/functions/s21_determinant.cc
+++ src/functions/s21_determinant.cc
@@ -2,81 +2,86 @@
 
 namespace s21 {
 
-double S21Matrix::Determinant() const {
-  CheckEmptyMatrix(*this);
-  CheckSquareMatrix();
+    double S21Matrix::Determinant() const {
+        CheckEmptyMatrix(*this);
+        CheckSquareMatrix();
 
-  S21Matrix temp = *this;
-  double result = 0;
+        S21Matrix temp = *this;
+        double result = 0;
 
-  int swap_count = GaussElimination(temp);
+        int swap_count = GaussElimination(temp);
 
-  double temp_result = 1;
+        double temp_result = 1;
 
-  for (int pivot_index = 0; pivot_index < rows_; ++pivot_index) {
-    temp_result *= temp.matrix_[pivot_index][pivot_index];
-  }
+        for (int pivot_index = 0; pivot_index < rows_; ++pivot_index) {
+            temp_result *= temp.matrix_[pivot_index][pivot_index];
+        }
 
-  double sign = GetSignBySwapCount(swap_count);
+        double sign = GetSignBySwapCount(swap_count);
 
-  if (!std::isnan(temp_result)) {
-    result = sign * temp_result;
-  }
-  if (IsZero(temp_result)) {
-    result = 0;
-  }
+        if (!std::isnan(temp_result)) {
+            result = sign * temp_result;
+        }
+        if (IsZero(temp_result)) {
+            result = 0;
+        }
 
-  return result;
-}
-
-void S21Matrix::PartialPivotingInGaussElimination(S21Matrix &temp,
-                                                  int pivot_index,
-                                                  int *p_swap_count) const {
-  for (int row_index = pivot_index + 1; row_index < temp.rows_; row_index++) {
-    double pivot_number = temp.matrix_[pivot_index][pivot_index];
-    double row_number = temp.matrix_[row_index][pivot_index];
-
-    if (std::fabs(pivot_number) < std::fabs(row_number)) {
-      SwapRows(temp, pivot_index, row_index);
-      (*p_swap_count)++;
+        return result;
     }
-  }
-}
 
-void S21Matrix::PerformGaussElimination(S21Matrix &temp,
-                                        int pivot_index) const {
-  for (int rowIndex = pivot_index + 1; rowIndex < rows_; ++rowIndex) {
-    double rowNumber = temp.matrix_[rowIndex][pivot_index];
-    double pivotNumber = temp.matrix_[pivot_index][pivot_index];
-    double term = rowNumber / pivotNumber;
-    for (int colIndex = 0; colIndex < rows_; colIndex++) {
-      double colNumber = temp.matrix_[pivot_index][colIndex];
-      double newColNumber = term * colNumber;
-      temp.matrix_[rowIndex][colIndex] -= newColNumber;
+    void S21Matrix::PartialPivotingInGaussElimination(S21Matrix &temp,
+                                                      int pivot_index,
+                                                      int *p_swap_count) const {
+        for (int row_index = pivot_index + 1; row_index < temp.rows_; row_index++) {
+            double pivot_number = temp.matrix_[pivot_index][pivot_index];
+            double row_number = temp.matrix_[row_index][pivot_index];
+
+            if (std::fabs(pivot_number) < std::fabs(row_number)) {
+                SwapRows(temp, pivot_index, row_index);
+                (*p_swap_count)++;
+            }
+        }
     }
-  }
-}
 
-int S21Matrix::GaussElimination(S21Matrix &temp) const {
-  int swap_count = 0;
-  for (int pivot_index = 0; pivot_index < temp.rows_ - 1; pivot_index++) {
-    PartialPivotingInGaussElimination(temp, pivot_index, &swap_count);
-    PerformGaussElimination(temp, pivot_index);
-  }
-  return swap_count;
-}
+    void S21Matrix::PerformGaussElimination(S21Matrix &temp,
+                                            int pivot_index) const {
+        for (int rowIndex = pivot_index + 1; rowIndex < rows_; ++rowIndex) {
+            double rowNumber = temp.matrix_[rowIndex][pivot_index];
+            double pivotNumber = temp.matrix_[pivot_index][pivot_index];
+            double term = rowNumber / pivotNumber;
+            for (int colIndex = 0; colIndex < rows_; colIndex++) {
+                double colNumber = temp.matrix_[pivot_index][colIndex];
+                double newColNumber = term * colNumber;
+                temp.matrix_[rowIndex][colIndex] -= newColNumber;
+            }
+        }
+    }
 
-void S21Matrix::SwapRows(S21Matrix &temp, int row_index_1,
-                         int row_index_2) const {
-  for (int columnIndex = 0; columnIndex < temp.rows_; columnIndex++) {
-    double tempValue = temp.matrix_[row_index_1][columnIndex];
-    temp.matrix_[row_index_1][columnIndex] =
-        temp.matrix_[row_index_2][columnIndex];
-    temp.matrix_[row_index_2][columnIndex] = tempValue;
-  }
-}
+    int S21Matrix::GaussElimination(S21Matrix &temp) const {
+        int swap_count = 0;
+        for (int pivot_index = 0; pivot_index < temp.rows_ - 1; pivot_index++) {
+            PartialPivotingInGaussElimination(temp, pivot_index, &swap_count);
+            PerformGaussElimination(temp, pivot_index);
+        }
+        return swap_count;
+    }
 
-double S21Matrix::GetSignBySwapCount(int swapCount) const {
-  return swapCount % 2 == 1 ? -1.0 : 1.0;
-}
+    void S21Matrix::SwapRows(S21Matrix &temp, int row_index_1,
+                             int row_index_2) const {
+        for (int col_index = 0; col_index < temp.rows_; col_index++) {
+            double &secondNumber = temp.matrix_[row_index_2][col_index];
+            double &firstNumber = temp.matrix_[row_index_1][col_index];
+            std::swap(firstNumber, secondNumber);
+        }
+    }
+
+    double S21Matrix::GetSignBySwapCount(int swapCount) const {
+
+        double result = 1.0;
+        bool is_odd_number = swapCount % 2 == 1;
+        if (is_odd_number) {
+            result = -1.0;
+        }
+        return result;
+    }
 }  // namespace s21
